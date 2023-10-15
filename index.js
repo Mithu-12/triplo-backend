e59178cd3ca3fe94f01993b30b0fd413bd658301
Mithu Vowmick <mithuvowmick96@gmail.com>
2023-10-15 05:20:33 -0400

    session-time-update

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ import packageRoute from './routes/package.js';
 import visaRoute from './routes/visa.js';
 import bookSchedule from './routes/BookSchedule.js';
 import airportRoute from './routes/airport.js';
-import paymentRoute from './routes/payment.js'
-import travelerRoute from './routes/traveler.js'
+import paymentRoute from './routes/payment.js';
+import travelerRoute from './routes/traveler.js';
 import bodyParser from 'body-parser';
 import passport from 'passport';
 import './config/passportConfig.js';
@@ -19,65 +19,87 @@ import cookieParser from 'cookie-parser';
 import crypto from 'crypto';
 import cors from 'cors';
 
-
-
 const app = express();
 dotenv.config();
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
 
-  res.setHeader('Content-Type', 'application/json');
+// Set up CORS middleware
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: 'GET, POST, DELETE, PUT',
+  credentials: true,
+}));
 
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+// Set up middleware to enable JSON parsing
+app.use(express.json());
 
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-});
+// Set up session middleware
+const secretKey = crypto.randomBytes(32).toString('hex');
+app.use(
+  session({
+    secret: secretKey,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 20 },
+  })
+);
 
+// Set up body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Initialize and configure Passport for authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Logging for MongoDB connection
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGODB);
-    console.log('connected to mongodb');
+    console.log('Connected to MongoDB');
   } catch (error) {
-    throw error;
+    console.error('Error connecting to MongoDB:', error);
   }
 };
 
 mongoose.set('debug', true);
 mongoose.connection.on('disconnected', () => {
-  console.log('mongodb disconnected');
+  console.log('MongoDB disconnected');
 });
 mongoose.connection.on('connected', () => {
-  console.log('mongodb connected');
+  console.log('MongoDB connected');
 });
 
-const secretKey = crypto.randomBytes(32).toString('hex');
+// Session timeout middleware
+const sessionTimeOut = 20 * 60 * 1000; // 20 minutes
+app.use((req, res, next) => {
+  if (req.session.createdAt) {
+    const currentTime = new Date().getTime();
+    const sessionTime = new Date(req.session.createdAt).getTime();
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    methods: 'GET, POST, DELETE, PUT',
-    credentials: true,
-  })
-  );
-app.use(express.json());
-app.use(
-  session({
-    secret: secretKey,
-    resave: true,
-    cookie: { maxAge: 1000 * 60 * 20 },
-    saveUninitialized: true,
-  })
-);
+    // Calculate the session duration
+    const sessionDuration = currentTime - sessionTime;
 
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+    // If the session duration exceeds the timeout, destroy the session
+    if (sessionDuration > sessionTimeOut) {
+      req.session.destroy((error) => {
+        if (error) {
+          console.error('Error destroying session', error);
+        }
+      });
+    }
+  }
 
-app.use(passport.initialize());
-app.use(passport.session());
+  // Set the current time as the session creation time
+  req.session.createdAt = new Date().toISOString();
 
+  // Pass sessionEndTime to the frontend (current time + session timeout)
+  req.session.sessionEndTime = new Date(Date.now() + sessionTimeOut).toISOString();
 
+  next();
+});
+
+// Configure routes
 app.use('/api/auth', authRoute);
 app.use('/api/hotels', hotelsRoute);
 app.use('/api/users', usersRoute);
@@ -85,25 +107,14 @@ app.use('/api/flight', flightRoute);
 app.use('/api/package', packageRoute);
 app.use('/api/visa', visaRoute);
 app.use('/api/bookSchedule', bookSchedule);
-app.use('/api/traveler', travelerRoute)
-app.use('/api/payment', paymentRoute)
-app.use('/api/auth', (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-});
-app.use('/api/airport', (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  next();
-});
+app.use('/api/traveler', travelerRoute);
+app.use('/api/payment', paymentRoute);
 app.use('/api/airport', airportRoute);
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   const errStatus = err.status || 500;
-  const errMessage = err.message || 'something wrong';
+  const errMessage = err.message || 'Something went wrong';
   return res.status(errStatus).json({
     success: false,
     message: errMessage,
@@ -112,33 +123,186 @@ app.use((err, req, res, next) => {
   });
 });
 
-
-const sessionTimeOut = 20 * 60 * 1000;
-
-app.use((req, res, next)=>{
-  if(req.session.createdAt){
-    const currentTime = new Date().getTime();
-    const sessionTime = new Date(req.session.createdAt).getTime();
-
-    if(currentTime -sessionTime > sessionTimeOut){
-      req.session.destroy((error)=>{
-        if(error){
-          console.error('Error destroying session', error)
-        }
-      })
-    }
-  }
-  req.session.createdAt = new Date().toISOString() 
-  next()
-})
-
-
-
+// Root route
 app.get('/', (req, res) => {
   res.send('Hello, this is the root route!');
 });
 
+// Start the server
 app.listen(8800, () => {
   connect();
-  console.log('backend is connected');
+  console.log('Backend is connected');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import mongoose from 'mongoose';
+// import authRoute from './routes/auth.js';
+// import hotelsRoute from './routes/hotels.js';
+// import usersRoute from './routes/users.js';
+// import flightRoute from './routes/flight.js';
+// import packageRoute from './routes/package.js';
+// import visaRoute from './routes/visa.js';
+// import bookSchedule from './routes/BookSchedule.js';
+// import airportRoute from './routes/airport.js';
+// import paymentRoute from './routes/payment.js'
+// import travelerRoute from './routes/traveler.js'
+// import bodyParser from 'body-parser';
+// import passport from 'passport';
+// import './config/passportConfig.js';
+// import session from 'express-session';
+// import cookieParser from 'cookie-parser';
+// import crypto from 'crypto';
+// import cors from 'cors';
+
+
+
+// const app = express();
+// dotenv.config();
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+
+//   res.setHeader('Content-Type', 'application/json');
+
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   next();
+// });
+
+// const connect = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB);
+//     console.log('connected to mongodb');
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+// mongoose.set('debug', true);
+// mongoose.connection.on('disconnected', () => {
+//   console.log('mongodb disconnected');
+// });
+// mongoose.connection.on('connected', () => {
+//   console.log('mongodb connected');
+// });
+
+// const secretKey = crypto.randomBytes(32).toString('hex');
+
+// app.use(
+//   cors({
+//     origin: 'http://localhost:5173',
+//     methods: 'GET, POST, DELETE, PUT',
+//     credentials: true,
+//   })
+//   );
+// app.use(express.json());
+// app.use(
+//   session({
+//     secret: secretKey,
+//     resave: true,
+//     cookie: { maxAge: 1000 * 60 * 20 },
+//     saveUninitialized: true,
+//   })
+// );
+
+// app.use(bodyParser.json());
+// app.use(cookieParser());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+
+// app.use('/api/auth', authRoute);
+// app.use('/api/hotels', hotelsRoute);
+// app.use('/api/users', usersRoute);
+// app.use('/api/flight', flightRoute);
+// app.use('/api/package', packageRoute);
+// app.use('/api/visa', visaRoute);
+// app.use('/api/bookSchedule', bookSchedule);
+// app.use('/api/traveler', travelerRoute)
+// app.use('/api/payment', paymentRoute)
+// app.use('/api/auth', (req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   next();
+// });
+// app.use('/api/airport', (req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   next();
+// });
+// app.use('/api/airport', airportRoute);
+
+// app.use((err, req, res, next) => {
+//   const errStatus = err.status || 500;
+//   const errMessage = err.message || 'something wrong';
+//   return res.status(errStatus).json({
+//     success: false,
+//     message: errMessage,
+//     status: errStatus,
+//     stack: err.stack,
+//   });
+// });
+
+
+// const sessionTimeOut = 20 * 60 * 1000; // 20 minutes
+
+// app.use((req, res, next) => {
+//   if (req.session.createdAt) {
+//     const currentTime = new Date().getTime();
+//     const sessionTime = new Date(req.session.createdAt).getTime();
+
+//     // Calculate the session duration
+//     const sessionDuration = currentTime - sessionTime;
+
+//     // If the session duration exceeds the timeout, destroy the session
+//     if (sessionDuration > sessionTimeOut) {
+//       req.session.destroy((error) => {
+//         if (error) {
+//           console.error('Error destroying session', error);
+//         }
+//       });
+//     }
+//   }
+
+//   // Set the current time as the session creation time
+//   req.session.createdAt = new Date().toISOString();
+
+//   // Pass sessionEndTime to the frontend (current time + session timeout)
+//   req.session.sessionEndTime = new Date(Date.now() + sessionTimeOut).toISOString();
+
+//   next();
+// });
+
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, this is the root route!');
+// });
+
+// app.listen(8800, () => {
+//   connect();
+//   console.log('backend is connected');
+// });
--- routes/airport.js
+++ routes/airport.js
@@ -1,22 +1,53 @@
 import express from 'express';
 import { searchFlights } from '../controller/airport.js';
-import { v4 as uuidv4 } from 'uuid'
-
+import { v4 as uuidv4 } from 'uuid';
+import winston from 'winston';
 
 const router = express.Router();
 
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+});
 
+const logFlightSearch = (req, res, next) => {
+  const searchUid = uuidv4();
+  req.session.searchUid = searchUid;
+  const searchParameters = {
+    originLocationCode: req.query.originLocationCode,
+    destinationLocationCode: req.query.destinationLocationCode,
+    departureDate: req.query.departureDate,
+    adults: req.query.adults,
+    returnDate: req.query.returnDate,
+    travelClass: req.query.travelClass,
+    children: req.query.children,
+    infants: req.query.infants,
+    max: req.query.max,
+    // Capture other search parameters as needed
+  };
+  const timestamp = new Date().toISOString();
+  logger.info('Flight Search Log:', { searchUid, searchParameters, timestamp });
+  // Log this data to your logging service
+  // You can replace this with your actual logging mechanism
+  console.log('Flight Search Log:', { searchUid, searchParameters, timestamp });
 
-// export default router;
+  next();
+};
+
+// Use the logFlightSearch middleware before your flight search route
+router.use('/search', logFlightSearch);
+
 router.get('/search', async (req, res, next) => {
   try {
+    // Use the sessionStartTime if available
+    const sessionStartTime = req.session.createdAt;
+    const sessionEndTime = req.session.sessionEndTime;
 
-    const searchUid = uuidv4();
-    const itineraryUid = uuidv4();
+    const searchUid = req.session.searchUid;
 
-    // Store them in the user's session
-    req.session.searchUid = searchUid;
-    req.session.itineraryUid = itineraryUid;
     // Retrieve the parameters from the query string
     const originLocationCode = req.query.originLocationCode;
     const destinationLocationCode = req.query.destinationLocationCode;
@@ -40,8 +71,26 @@ router.get('/search', async (req, res, next) => {
       max,
     });
 
-    console.log('amadeus data', flights);
-    res.json(flights);
+    logger.info('Flight Search Data', {
+      searchUid,
+      sessionStartTime,
+      sessionEndTime,
+      originLocationCode,
+      destinationLocationCode,
+      departureDate,
+      adults,
+      returnDate,
+      travelClass,
+      children,
+      infants,
+      max,
+    });
+
+    console.log('Amadeus data', flights);
+    
+    
+    // Send the sessionStartTime and sessionEndTime to the frontend
+    res.json({ flights, sessionStartTime, sessionEndTime, searchUid });
   } catch (error) {
     console.error('An error occurred while searching flights:', error);
     next(error);
@@ -54,15 +103,18 @@ export default router;
 // import { searchFlights } from '../controller/airport.js';
 // import { v4 as uuidv4 } from 'uuid'
 
-
 // const router = express.Router();
-
-
 
 // // export default router;
 // router.get('/search', async (req, res, next) => {
 //   try {
 
+//     const searchUid = uuidv4();
+//     const itineraryUid = uuidv4();
+//     req.session.searchStartTime = new Date().getTime();
+//     // Store them in the user's session
+//     req.session.searchUid = searchUid;
+//     req.session.itineraryUid = itineraryUid;
 
 //     // Retrieve the parameters from the query string
 //     const originLocationCode = req.query.originLocationCode;
@@ -86,9 +138,10 @@ export default router;
 //       infants,
 //       max,
 //     });
-
+//     const currentTime = new Date().toISOString();
+//     console.log('searchStartTime', searchStartTime)
 //     console.log('amadeus data', flights);
-//     res.json(flights);
+//     res.json({flights, currentTime });
 //   } catch (error) {
 //     console.error('An error occurred while searching flights:', error);
 //     next(error);
@@ -96,4 +149,3 @@ export default router;
 // });
 
 // export default router;
-
