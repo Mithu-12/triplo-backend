05b81e6cbd03d79fa79c9ba4fa6da160f8505680
mithu-12 <mithuvowmick96@gmail.com>
2023-10-25 16:47:50 +0600

    google update4

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -12,7 +12,7 @@ passport.use(
       // Google OAuth2 credentials
       clientID: process.env.GOOGLE_CLIENT_API_KEY,
       clientSecret: process.env.GOOGLE_SECRET_API_KEY,
-      callbackURL: 'https://triplo.cyclic.app/api/auth/google/callback',
+      callbackURL: '/api/auth/google/callback',
       
     },
     async (req, accessToken, refreshToken, profile, done) => {
@@ -77,6 +77,7 @@ passport.use(
 passport.serializeUser((user, done) => {
   done(null, user);
 });
+
 passport.deserializeUser((user, done) => {
   done(null, user);
 });
