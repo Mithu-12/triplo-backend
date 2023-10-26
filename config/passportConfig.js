e4b07849c3185a65092ec992d31d95a694b02844
mithu-12 <mithuvowmick96@gmail.com>
2023-10-26 12:43:45 +0600

    google update7

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -1,28 +1,20 @@
-// // backend/config/passportConfig.js
+// passport-config.js
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import { Strategy as FacebookStrategy } from 'passport-facebook';
-import User from '../models/User.js';
-
-
+import User from './models/User.js';
 
 passport.use(
   new GoogleStrategy(
     {
-      // Google OAuth2 credentials
       clientID: process.env.GOOGLE_CLIENT_API_KEY,
       clientSecret: process.env.GOOGLE_SECRET_API_KEY,
       callbackURL: 'https://triplo.cyclic.app/api/auth/google/callback',
-      
     },
-    async (req, accessToken, refreshToken, profile, done) => {
-      console.log('Google OAuth2 strategy called');
+    async (accessToken, refreshToken, profile, done) => {
       try {
-        // Check if the user already exists in database
         let user = await User.findOne({ googleId: profile.id });
-        // console.log(profile);
+
         if (!user) {
-          // Create a new user if not found
           user = await User.create({
             googleId: profile.id,
             name: profile.displayName,
@@ -31,41 +23,103 @@ passport.use(
             userName: profile.emails[0].value,
           });
         }
-       
 
-        // Call done with null for the error and the user object
         return done(null, user);
       } catch (error) {
-        // Call done with the error object and false for the user
-        console.log('new error', error)
+        console.error('Error:', error);
         return done(error, false);
       }
     }
   )
 );
 
-
-
-
 passport.serializeUser((user, done) => {
-  done(null, user);
+  done(null, user._id);
 });
 
-passport.deserializeUser((user, done) => {
-  done(null, user);
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    console.error('Error:', error);
+    done(error, null);
+  }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // backend/config/passportConfig.js
+// import passport from 'passport';
+// import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+// import { Strategy as FacebookStrategy } from 'passport-facebook';
+// import User from '../models/User.js';
+
+
+
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       // Google OAuth2 credentials
+//       clientID: process.env.GOOGLE_CLIENT_API_KEY,
+//       clientSecret: process.env.GOOGLE_SECRET_API_KEY,
+//       callbackURL: 'https://triplo.cyclic.app/api/auth/google/callback',
+      
+//     },
+//     async (req, accessToken, refreshToken, profile, done) => {
+//       console.log('Google OAuth2 strategy called');
+//       try {
+//         // Check if the user already exists in database
+//         let user = await User.findOne({ googleId: profile.id });
+//         // console.log(profile);
+//         if (!user) {
+//           // Create a new user if not found
+//           user = await User.create({
+//             googleId: profile.id,
+//             name: profile.displayName,
+//             email: profile.emails[0].value,
+//             picture: profile.photos[0].value,
+//             userName: profile.emails[0].value,
+//           });
+//         }
+       
+
+//         // Call done with null for the error and the user object
+//         return done(null, user);
+//       } catch (error) {
+//         // Call done with the error object and false for the user
+//         console.log('new error', error)
+//         return done(error, false);
+//       }
+//     }
+//   )
+// );
+
+
 
 
 // passport.serializeUser((user, done) => {
-//   done(null, user.id); // Store user id in the session
+//   done(null, user);
 // });
 
-// passport.deserializeUser((id, done) => {
-//   // Retrieve user from the id stored in the session
-//   User.findById(id, (err, user) => {
-//     done(err, user);
-//   });
+// passport.deserializeUser((user, done) => {
+//   done(null, user);
 // });
+
 
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -72,42 +72,149 @@ router.post('/logout', function (req, res, next) {
   });
 });
 
+// Initialize the Google OAuth2 authentication process
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+// Handle the callback from Google OAuth2
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+  // Redirect to your desired route upon successful authentication
+  res.redirect('/login/success');
+});
+
 router.get(
-  '/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
+  '/facebook',
+  passport.authenticate('facebook', { scope: ['email'] })
+);
+router.get(
+  '/facebook/callback',
+  passport.authenticate('facebook'),
+  (req, res) => {
+    const token = generateToken(req.user._id);
+    res.cookie('access_token', token, { httpOnly: true });
+    res.json({ user: req.user });
+  }
 );
 
-router.get('/google/callback', (req, res, next) => {
-  passport.authenticate('google', async (err, user, info) => {
-    try {
-      if (err) {
-        throw err; // Handle the error
-      }
+export default router;
 
-      if (!user) {
-        // Authentication failed
-        return res.redirect('/failure');
-      }
 
-      // Authentication succeeded, now store the user in session
-      req.login(user, async (loginErr) => {
-        if (loginErr) {
-          throw loginErr; // Handle the login error
-        }
-        req.session.user = req.user;
-        const sessionUser = req.session.user;
-        console.log('session', sessionUser)
-        // return res.redirect(SUCCESS_URL)
-        return res.redirect(SUCCESS_URL);
-      });
-    } catch (error) {
-      console.error('Error:', error);
-      res
-        .status(500)
-        .json({ success: false, message: 'Internal server error' });
-    }
-  })(req, res, next);
-});
+
+
+
+
+
+
+
+
+
+// import express from 'express';
+// import passport from 'passport';
+// import { changePassword, login, register } from '../controller/auth.js';
+// import generateToken from '../utils/generateToken.js';
+// import { verifyToken, verifyUser } from '../utils/verifyToken.js';
+// const router = express.Router();
+// const CLIENT_URL = 'http://localhost:5173';
+// const SUCCESS_URL = 'http://localhost:5173/login/success';
+// // const SUCCESS_URL = 'https://triplo-flights.vercel.app/login/success';
+
+// router.post('/register', register);
+
+// router.post('/login', login);
+// router.post('/change-password',  changePassword);
+
+
+// router.get('/login/success', async (req, res) => {
+//   try {
+//     // Use the user data stored in the session
+//     const sessionUser = req.session.user;
+// console.log('user', sessionUser)
+//     if (!sessionUser) {
+//       return res.status(401).json({
+//         success: false,
+//         message: 'Unauthorized',
+//       });
+//     }
+
+//     // Generate an access token (JWT)
+//     const token = generateToken(sessionUser._id);
+
+//     // Set the access token as a cookie
+//     res.cookie('access_token', token, { httpOnly: true });
+
+//     // Send the access token and user in the response
+//     res.status(200).json({
+//       success: true,
+//       message: 'success',
+//       access_token: token,
+//       user: sessionUser,
+//     });
+//   } catch (error) {
+//     // Handle any errors that occur during this process
+//     console.error('Error:', error);
+//     res.status(500).json({
+//       success: false,
+//       message: 'Internal server error',
+//     });
+//   }
+// });
+
+// router.get('/login/failed', (req, res) => {
+//   res.status(401).json({
+//     success: false,
+//     message: 'failure',
+//   });
+// });
+
+// router.post('/logout', function (req, res, next) {
+//   // Call the req.logout() function with a callback function
+//   req.logout(function (err) {
+//     if (err) {
+//       // Handle error if logout fails
+//       console.error('Logout error:', err);
+//       return res.status(500).json({ message: 'Logout failed' });
+//     }
+
+//     // Redirect to the desired route after successful logout
+//     res.redirect('/');
+//   });
+// });
+
+// router.get(
+//   '/google',
+//   passport.authenticate('google', { scope: ['profile', 'email'] })
+// );
+
+// router.get('/google/callback', (req, res, next) => {
+//   passport.authenticate('google', async (err, user, info) => {
+//     try {
+//       if (err) {
+//         throw err; // Handle the error
+//       }
+
+//       if (!user) {
+//         // Authentication failed
+//         return res.redirect('/failure');
+//       }
+
+//       // Authentication succeeded, now store the user in session
+//       req.login(user, async (loginErr) => {
+//         if (loginErr) {
+//           throw loginErr; // Handle the login error
+//         }
+//         req.session.user = req.user;
+//         const sessionUser = req.session.user;
+//         console.log('session', sessionUser)
+//         // return res.redirect(SUCCESS_URL)
+//         return res.redirect(SUCCESS_URL);
+//       });
+//     } catch (error) {
+//       console.error('Error:', error);
+//       res
+//         .status(500)
+//         .json({ success: false, message: 'Internal server error' });
+//     }
+//   })(req, res, next);
+// });
 
 
 // router.get(
@@ -124,7 +231,7 @@ router.get('/google/callback', (req, res, next) => {
 //   }
 // );
 
-export default router;
+// export default router;
 
 
 
