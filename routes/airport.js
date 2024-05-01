988ca0f4562b1cfe74c117db3426c57e2d9993d7
mithu-12 <mithuvowmick96@gmail.com>
2024-05-01 11:15:54 +0600

    flight-search-update

--- routes/airport.js
+++ routes/airport.js
@@ -1,17 +1,8 @@
 import express from 'express';
 import { searchFlights } from '../controller/airport.js';
 import { v4 as uuidv4 } from 'uuid';
-import winston from 'winston';
 
 const router = express.Router();
-
-const logger = winston.createLogger({
-  transports: [new winston.transports.Console()],
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-});
 
 const logFlightSearch = (req, res, next) => {
   const searchUid = uuidv4();
@@ -26,39 +17,28 @@ const logFlightSearch = (req, res, next) => {
     children: req.query.children,
     infants: req.query.infants,
     max: req.query.max,
-    // Capture other search parameters as needed
   };
-  const timestamp = new Date().toISOString();
-  logger.info('Flight Search Log:', { searchUid, searchParameters, timestamp });
-  // Log this data to your logging service
-  // You can replace this with your actual logging mechanism
-  console.log('Flight Search Log:', { searchUid, searchParameters, timestamp });
-
   next();
 };
 
-// Use the logFlightSearch middleware before your flight search route
 router.use('/search', logFlightSearch);
 
 router.get('/search', async (req, res, next) => {
   try {
-    // Use the sessionStartTime if available
     const sessionStartTime = req.session.createdAt;
     const sessionEndTime = req.session.sessionEndTime;
-
     const searchUid = req.session.searchUid;
-
-    // Retrieve the parameters from the query string
-    const originLocationCode = req.query.originLocationCode;
-    const destinationLocationCode = req.query.destinationLocationCode;
-    const departureDate = req.query.departureDate;
-    const adults = req.query.adults;
-    const returnDate = req.query.returnDate;
-    const travelClass = req.query.travelClass;
-    const children = req.query.children;
-    const infants = req.query.infants;
-    const max = req.query.max;
-
+    const {
+      originLocationCode,
+      destinationLocationCode,
+      departureDate,
+      adults,
+      returnDate,
+      travelClass,
+      children,
+      infants,
+      max
+    } = req.query;
     const flights = await searchFlights({
       originLocationCode,
       destinationLocationCode,
@@ -70,26 +50,6 @@ router.get('/search', async (req, res, next) => {
       infants,
       max,
     });
-
-    logger.info('Flight Search Data', {
-      searchUid,
-      sessionStartTime,
-      sessionEndTime,
-      originLocationCode,
-      destinationLocationCode,
-      departureDate,
-      adults,
-      returnDate,
-      travelClass,
-      children,
-      infants,
-      max,
-    });
-
-    console.log('Amadeus data', flights);
-    
-    
-    // Send the sessionStartTime and sessionEndTime to the frontend
     res.json({ flights, sessionStartTime, sessionEndTime, searchUid });
   } catch (error) {
     console.error('An error occurred while searching flights:', error);
@@ -98,4 +58,3 @@ router.get('/search', async (req, res, next) => {
 });
 
 export default router;
-
